06dcf6528b2f07ea77e1cfd375e29b6c8f599ace
Jash Jogi <jash.rideholic@gmail.com>
2022-06-13 18:17:36 +0530

    Initial Commit

---
+++ Shared/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  Shared
+//
+//  Created by Jash on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
